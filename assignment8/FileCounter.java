47ba95fb9c3fe097910050c4dcb03f67d96d51e8
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-12 22:09:09 -0800

    score update

--- assignment8/FileCounter.java
+++ assignment8/FileCounter.java
@@ -2,7 +2,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class FileCounter {
+public class FileCounter { // score 3
     private int characterCount, wordCount, lineCount;
 
     public FileCounter(){
